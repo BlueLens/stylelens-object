6fdab6995c617168d7865ab251842ac829e2598d
Youngbok Yoon <master@bluehack.net>
2017-12-18 12:36:41 +0900

    [BL-240] Initial setup

---
+++ sample/add_object.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+from stylelens_object.objects import Objects
+from pprint import pprint
+# create an instance of the API class
+api_instance = Objects()
+
+object = {}
+object['product_id'] = '1234'
+
+try:
+  # Added a new Object
+  api_response = api_instance.add_object(object)
+  pprint(api_response)
+except Exception as e:
+  print("Exception when calling ProductApi->add_object: %s\n" % e)
